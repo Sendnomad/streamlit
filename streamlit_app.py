4df1f54521ea3f8e6a3110cd875326fcbb3ef4dc
Azamat Ryskulbekov <54107853+azadatas@users.noreply.github.com>
2023-12-04 13:16:34 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,49 +1,56 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
 from google.cloud import firestore
-import random
-from datetime import datetime
 
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestore-key.json")
 
-# Function to generate random data
-def generate_random_data():
-    transaction_id = st.session_state.current_transaction_id
-    st.session_state.current_transaction_id += 1
-    
-    crypto_received = random.uniform(30, 3000)
-    pct_margin = random.uniform(0, 5)
-    margin = (crypto_received * pct_margin) / 100
-    crypto_spent = crypto_received - margin
+# Function to retrieve data from Firestore
+def get_data():
+    collection_ref = db.collection("your_collection_name")
+    docs = collection_ref.stream()
+    data = [doc.to_dict() for doc in docs]
+    return data
 
-    return {
-        'transaction_id': transaction_id,
-        'crypto_received': crypto_received,
-        'crypto_spent': crypto_spent,
-        'margin': margin,
-        'pct_margin': pct_margin,
-        'time': datetime.now(),
-    }
+# Function to create a dataframe from the retrieved data
+def create_dataframe():
+    data = get_data()
+    df = pd.DataFrame(data)
+    return df
 
-# Create a reference to the Firestore collection
-collection_ref = db.collection("your_collection_name")
+# Function to visualize with Matplotlib
+def visualize_with_matplotlib(df):
+    st.subheader("Visualization with Matplotlib")
+    fig, ax = plt.subplots()
+    ax.plot(df['transaction_id'], df['crypto_received'], label='Crypto Received')
+    ax.plot(df['transaction_id'], df['crypto_spent'], label='Crypto Spent')
+    ax.set_xlabel('Transaction ID')
+    ax.set_ylabel('Amount')
+    ax.legend()
+    st.pyplot(fig)
 
-# Initialize session state to keep track of transaction_id
-if 'current_transaction_id' not in st.session_state:
-    st.session_state.current_transaction_id = 0
+# Function to visualize with Plotly
+def visualize_with_plotly(df):
+    st.subheader("Visualization with Plotly")
+    fig = px.line(df, x='transaction_id', y=['crypto_received', 'crypto_spent'],
+                 labels={'value': 'Amount', 'variable': 'Type'},
+                 title='Crypto Transactions Over Time')
+    st.plotly_chart(fig)
 
-# Get the number of random records to insert
-num_records = st.slider("Select the number of records to insert:", 1, 100, 10)
+# Streamlit App
+def main():
+    st.title("Crypto Transactions Visualization")
 
-# Insert random data into Firestore
-for _ in range(num_records):
-    data = generate_random_data()
-    collection_ref.add(data)
+    # Retrieve data from Firestore and create a dataframe
+    df = create_dataframe()
 
-# Display a success message
-st.success(f'{num_records} random records inserted into Firestore.')
+    # Visualize with Matplotlib
+    visualize_with_matplotlib(df)
 
+    # Visualize with Plotly
+    visualize_with_plotly(df)
 
-# Display a success message
-st.success(f'{num_records} random records inserted into Firestore.')
-
+if __name__ == "__main__":
+    main()
