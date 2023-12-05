85455ac5472d772b122d2199db5bca8f65528169
Azamat Ryskulbekov <54107853+azadatas@users.noreply.github.com>
2023-12-05 13:33:07 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,25 +4,90 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 from google.cloud import firestore
 from datetime import datetime, timedelta
+import sqlite3
 
 # Authenticate to Firestore with the JSON account key.
 db = firestore.Client.from_service_account_json("firestore-key.json")
 
-def get_data():
+# SQLite database connection
+sqlite_conn = sqlite3.connect('firestore_cache.db')
+sqlite_cursor = sqlite_conn.cursor()
+
+def create_table_if_not_exists():
+    # Create the table if it doesn't exist
+    sqlite_cursor.execute('''
+        CREATE TABLE IF NOT EXISTS firestore_cache (
+            time TEXT PRIMARY KEY,
+            crypto_received REAL,
+            crypto_spent REAL,
+            margin REAL,
+            pct_margin REAL,
+            transaction_id TEXT
+        )
+    ''')
+    sqlite_conn.commit()
+
+def get_data_from_firestore():
     collection_ref = db.collection("your_collection_name")
     docs = collection_ref.stream()
     data = [doc.to_dict() for doc in docs]
     return data
 
+def get_data_from_sqlite():
+    sqlite_cursor.execute("SELECT * FROM firestore_cache")
+    data = sqlite_cursor.fetchall()
+    return data
+
+def update_sqlite_cache(data):
+    try:
+        values = [
+            (entry['time'], entry['crypto_received'], entry['crypto_spent'], entry['margin'], entry['pct_margin'], entry['transaction_id'])
+            for entry in data
+        ]
+    except KeyError as e:
+        st.error(f"KeyError: {e} is missing in data. Check your data structure.")
+        return
+
+    sqlite_cursor.executemany('''
+        INSERT OR IGNORE INTO firestore_cache (time, crypto_received, crypto_spent, margin, pct_margin, transaction_id)
+        VALUES (?, ?, ?, ?, ?, ?)
+    ''', values)
+    sqlite_conn.commit()
+
+
 def create_dataframe():
-    data = get_data()
-    df = pd.DataFrame(data)
+    create_table_if_not_exists()
+    
+    # Fetch data from SQLite
+    sqlite_data = get_data_from_sqlite()
+
+    # If SQLite has data, fetch only missing data from Firestore
+    if sqlite_data:
+        firestore_data = get_missing_data_from_firestore(sqlite_data)
+    else:
+        # If SQLite is empty, fetch all data from Firestore
+        firestore_data = get_data_from_firestore()
+
+    # Update SQLite cache with new data
+    update_sqlite_cache(firestore_data + sqlite_data)
+
+    df = pd.DataFrame(firestore_data + sqlite_data)
     
     # Convert 'time' column to datetime type
     df['time'] = pd.to_datetime(df['time'])
     
     return df
 
+def get_missing_data_from_firestore(sqlite_data):
+    # Get the latest time from SQLite
+    latest_time = max(entry[0] for entry in sqlite_data)
+
+    # Fetch data from Firestore that is not in SQLite
+    collection_ref = db.collection("your_collection_name")
+    docs = collection_ref.where('time', '>', latest_time).stream()
+    data = [doc.to_dict() for doc in docs]
+    
+    return data
 
 def visualize_with_matplotlib(df):
     st.subheader("Scatter Plot: Transaction Amount vs. Percentage Margin")
@@ -44,33 +109,6 @@ def visualize_with_plotly(df):
                      title='Transaction Amount vs. Percentage Margin')
     st.plotly_chart(fig)
 
-def average_pct_margin_by_time_period(df, time_period):
-    now = datetime.now()
-
-    if time_period == 'Last 30 mins':
-        start_time = now - timedelta(minutes=30)
-    elif time_period == 'Last 1 hour':
-        start_time = now - timedelta(hours=1)
-    elif time_period == 'Last 10 hours':
-        start_time = now - timedelta(hours=10)
-    elif time_period == 'Last day':
-        start_time = now - timedelta(days=1)
-    elif time_period == 'Last week':
-        start_time = now - timedelta(weeks=1)
-    elif time_period == 'Last month':
-        start_time = now - timedelta(weeks=4)  # Approximating a month as 4 weeks
-    else:
-        st.error("Invalid time period selected.")
-        return None
-
-    # Convert 'time' and 'start_time' to datetime type
-    df['time'] = pd.to_datetime(df['time'], utc=True)
-    start_time = pd.to_datetime(start_time, utc=True)
-
-    filtered_df = df[(df['time'] >= start_time) & (df['time'] <= now)]
-    avg_pct_margin = filtered_df['pct_margin'].mean()
-
-    return avg_pct_margin
 
 def main():
     st.title("Crypto Transactions Visualization")
@@ -88,16 +126,8 @@ def main():
     # Visualize with Plotly
     visualize_with_plotly(df)
 
-    sum_margin_df = sum_margin_by_30_minutes(df)
-    st.subheader("Bar Plot: Sum of Margin for 30-minute Intervals")
-
     # Widget to select time period
     time_period = st.selectbox("Select Time Period", ['Last 30 mins', 'Last 1 hour', 'Last 10 hours', 'Last day', 'Last week', 'Last month'])
-
-    # Calculate and display average pct_margin for the selected time period
-    avg_pct_margin = average_pct_margin_by_time_period(df, time_period)
-    if avg_pct_margin is not None:
-        st.subheader(f"Average Percentage Margin for {time_period}: {avg_pct_margin:.2f}%")
 
 if __name__ == "__main__":
     main()
