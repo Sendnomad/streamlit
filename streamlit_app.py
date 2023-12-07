9be0c0357b7b6dfefb6b5b3f5cd2ffb72df10fa9
Azamat Ryskulbekov <54107853+azadatas@users.noreply.github.com>
2023-12-07 15:21:37 +0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,13 @@ import plotly.express as px
 from google.cloud import firestore
 from datetime import datetime, timedelta
 import sqlite3
+import json
+from google.oauth2 import service_account
 
 # Authenticate to Firestore with the JSON account key.
-db = firestore.Client.from_service_account_json("firestore-key.json")
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="streamlit-reddit")
 
 # SQLite database connection
 sqlite_conn = sqlite3.connect('firestore_cache.db')
@@ -22,10 +26,55 @@ def create_table_if_not_exists():
             crypto_spent REAL,
             margin REAL,
             pct_margin REAL,
-            transaction_id TEXT
+            transaction_id TEXT,
+            actualAmount REAL,
+            amount REAL,
+            blockchainTransferId TEXT,
+            blockchainaddress TEXT,
+            cashbackLosses REAL,
+            cashedOut INTEGER,
+            completed INTEGER,
+            cryptocurrency TEXT,
+            fromCurrency TEXT,
+            hasCashRewards INTEGER,
+            mercuryoFiatAmount REAL,
+            mercuryoFromCurrency TEXT,
+            nickname TEXT,
+            orderNumber TEXT,
+            p2pRateRealized REAL,
+            profit REAL,
+            profitActual REAL,
+            recipientAmount REAL,
+            recipientAmountDelivered TEXT,
+            recipientCurrency TEXT,
+            recipientPayoutOption TEXT,
+            sendCountry TEXT,
+            systemId TEXT,
+            time_created TEXT,
+            username TEXT,
+            valueCryptoReceived REAL,
+            valueCryptoUsed REAL,
+            valueCryptoUsedActual REAL
         )
     ''')
     sqlite_conn.commit()
+
+def firestore_to_python_type(value, data_type):
+    if value is not None:
+        if data_type == 'string':
+            # Check if it's a numeric string and convert to float if possible
+            try:
+                return float(value)
+            except ValueError:
+                # If conversion to float fails, return as string
+                return str(value)
+        elif data_type == 'number':
+            return float(value)
+        elif data_type == 'boolean':
+            return bool(value)
+        elif data_type == 'timestamp':
+            return datetime.utcfromtimestamp(value.seconds + value.nanos / 1e9)
+    return None  # Return None for empty values
 
 def get_data_from_firestore():
     collection_ref = db.collection("your_collection_name")
@@ -40,17 +89,55 @@ def get_data_from_sqlite():
 
 def update_sqlite_cache(data):
     try:
-        values = [
-            (entry['time'], entry['crypto_received'], entry['crypto_spent'], entry['margin'], entry['pct_margin'], entry['transaction_id'])
-            for entry in data
-        ]
+        values = [(entry['time'],
+                   firestore_to_python_type(entry.get('crypto_received'), 'number'),
+                   firestore_to_python_type(entry.get('crypto_spent'), 'number'),
+                   firestore_to_python_type(entry.get('margin'), 'number'),
+                   firestore_to_python_type(entry.get('pct_margin'), 'number'),
+                   entry.get('transaction_id'),
+                   firestore_to_python_type(entry.get('actualAmount'), 'number'),
+                   firestore_to_python_type(entry.get('amount'), 'number'),
+                   entry.get('blockchainTransferId'),
+                   entry.get('blockchainaddress'),
+                   firestore_to_python_type(entry.get('cashbackLosses'), 'number'),
+                   firestore_to_python_type(entry.get('cashedOut'), 'boolean'),
+                   firestore_to_python_type(entry.get('completed'), 'boolean'),
+                   entry.get('cryptocurrency'),
+                   entry.get('fromCurrency'),
+                   firestore_to_python_type(entry.get('hasCashRewards'), 'boolean'),
+                   firestore_to_python_type(entry.get('mercuryoFiatAmount'), 'number'),
+                   entry.get('mercuryoFromCurrency'),
+                   entry.get('nickname'),
+                   entry.get('orderNumber'),
+                   firestore_to_python_type(entry.get('p2pRateRealized'), 'number'),
+                   firestore_to_python_type(entry.get('profit'), 'number'),
+                   firestore_to_python_type(entry.get('profitActual'), 'number'),
+                   firestore_to_python_type(entry.get('recipientAmount'), 'number'),
+                   firestore_to_python_type(entry.get('recipientAmountDelivered'), 'number'),
+                   entry.get('recipientCurrency'),
+                   entry.get('recipientPayoutOption'),
+                   entry.get('sendCountry'),
+                   entry.get('systemId'),
+                   firestore_to_python_type(entry.get('time_created'), 'timestamp'),
+                   entry.get('username'),
+                   firestore_to_python_type(entry.get('valueCryptoReceived'), 'number'),
+                   firestore_to_python_type(entry.get('valueCryptoUsed'), 'number'),
+                   firestore_to_python_type(entry.get('valueCryptoUsedActual'), 'number'))
+                  for entry in data]
     except KeyError as e:
         st.error(f"KeyError: {e} is missing in data. Check your data structure.")
         return
 
     sqlite_cursor.executemany('''
-        INSERT OR IGNORE INTO firestore_cache (time, crypto_received, crypto_spent, margin, pct_margin, transaction_id)
-        VALUES (?, ?, ?, ?, ?, ?)
+        INSERT OR IGNORE INTO firestore_cache 
+        (time, crypto_received, crypto_spent, margin, pct_margin, transaction_id, 
+         actualAmount, amount, blockchainTransferId, blockchainaddress, cashbackLosses,
+         cashedOut, completed, cryptocurrency, fromCurrency, hasCashRewards, mercuryoFiatAmount,
+         mercuryoFromCurrency, nickname, orderNumber, p2pRateRealized, profit, profitActual,
+         recipientAmount, recipientAmountDelivered, recipientCurrency, recipientPayoutOption,
+         sendCountry, systemId, time_created, username, valueCryptoReceived, valueCryptoUsed,
+         valueCryptoUsedActual)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     ''', values)
     sqlite_conn.commit()
 
